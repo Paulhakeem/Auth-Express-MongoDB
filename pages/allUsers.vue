452433f664fc988f7ffb2260f4f7ad85e0687f14
Paulhakeem <paulnyamawi18@gmail.com>
2024-06-23 20:51:39 +0300

    creating users list

--- pages/allUsers.vue
+++ pages/allUsers.vue
@@ -2,7 +2,11 @@
   <div class="bg-[#090c28] h-screen">
     <div class="p-5 bg-[#07d884]"></div>
     <!-- users available -->
-    <users-online/>
+    <users-online />
+    <!--  -->
+    <div class="mt-14 h-10 bg-gray-200 justify-center w-96">
+
+    </div>
   </div>
 </template>
 
