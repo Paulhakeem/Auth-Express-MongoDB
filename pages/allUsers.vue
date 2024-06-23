3ff992172a77888762686b1739f457edfaba7a57
Paulhakeem <paulnyamawi18@gmail.com>
2024-06-23 19:22:22 +0300

    creating users darshboard

---
+++ pages/allUsers.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-[#090c28] h-screen">
+    <div class="p-5 bg-[#07d884]"></div>
+    <!-- users available -->
+    <users-online/>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
